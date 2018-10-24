1232bf7b41d4ffe338be96f71159edcd088a25ce
Khaled Sharif <kldsrf@gmail.com>
2018-10-24 12:16:38 -0700

    added bing maps build-time ARG and added migrations removal script

--- subsea/startup.sh
+++ subsea/startup.sh
@@ -1,6 +1,9 @@
 cd /root/xgds_subsea && \
-    ./manage.py prep && \
-    ./manage.py prepnpm
-    ./manage.py prepmigrations && \
-    ./manage.py migrate && \
-    exec /usr/sbin/apachectl -D FOREGROUND
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete && \
+find . -path "*/migrations/*.pyc"  -delete && \
+./manage.py prep && \
+./manage.py prepnpm && \
+./manage.py prepmigrations && \
+./manage.py migrate && \
+exec /usr/sbin/apachectl -D FOREGROUND
+
