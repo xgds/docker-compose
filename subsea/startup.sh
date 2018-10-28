c1ac062d0982e1a74c8ca9832feb456f2759c5f1
Khaled Sharif <kldsrf@gmail.com>
2018-10-28 13:26:47 -0700

    removed prepnpm

--- subsea/startup.sh
+++ subsea/startup.sh
@@ -2,7 +2,6 @@ cd /root/xgds_subsea && \
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete && \
 find . -path "*/migrations/*.pyc"  -delete && \
 ./manage.py prep && \
-./manage.py prepnpm && \
 ./manage.py prepmigrations && \
 ./manage.py migrate && \
 exec /usr/sbin/apachectl -D FOREGROUND
