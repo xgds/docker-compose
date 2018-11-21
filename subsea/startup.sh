1ea5aa522860ff813658aab55758e2ea69af4d1d
Tamar Cohen <tamar.cohen@nasa.gov>
2018-11-21 11:08:31 -0800

    updating couch in docker compose; updated to 3.7

--- subsea/startup.sh
+++ subsea/startup.sh
@@ -1,14 +1,16 @@
-#curl -X PUT http://couchdb:5984/_users
-#curl -X PUT http://couchdb:5984/_replicator
-#curl -X PUT http://couchdb:5984/_global_changes
-#curl -X PUT http://couchdb:5984/subsea-file-store
 
 cd /root/xgds_subsea && \
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete && \
 find . -path "*/migrations/*.pyc"  -delete && \
 ./manage.py prep && \
 ./manage.py prepmigrations && \
-./manage.py migrate && \
+./manage.py migrate
+
+curl -X PUT http://couchdb:5984/_users
+curl -X PUT http://couchdb:5984/_replicator
+curl -X PUT http://couchdb:5984/_global_changes
+curl -X PUT http://couchdb:5984/subsea-file-store
+
 exec /usr/sbin/apachectl -D FOREGROUND
 tail -f /var/logs/apache2/error.log
 
